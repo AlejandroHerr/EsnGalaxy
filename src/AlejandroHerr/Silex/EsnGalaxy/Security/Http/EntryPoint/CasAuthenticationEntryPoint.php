16b01c3d910f21e5bf11cf456b273ab8fe9d4c2a
AlejandroHerr <alejandrohnc88@gmail.com>
2015-01-19 14:03:48 +0100

    Extend FormAuthenticationEntryPoint

--- src/AlejandroHerr/Silex/EsnGalaxy/Security/Http/EntryPoint/CasAuthenticationEntryPoint.php
+++ src/AlejandroHerr/Silex/EsnGalaxy/Security/Http/EntryPoint/CasAuthenticationEntryPoint.php
@@ -1,24 +1,14 @@
 <?php
 namespace AlejandroHerr\Silex\EsnGalaxy\Security\Http\EntryPoint;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\Security\Http\HttpUtils;
+use Symfony\Component\Security\Http\EntryPoint\FormAuthenticationEntryPoint;
 
-class CasAuthenticationEntryPoint implements AuthenticationEntryPointInterface
+class CasAuthenticationEntryPoint extends FormAuthenticationEntryPoint
 {
-    protected $httpUtils;
-    protected $loginPath;
-
-    public function __construct(HttpUtils $httpUtils, array $options = array())
+    public function __construct(HttpKernelInterface $kernel, HttpUtils $httpUtils, $loginPath = '/login', $useForward = false)
     {
-        $this->httpUtils = $httpUtils;
-        $this->loginPath = isset($options['login_path']) ? $options['login_path'] : '/login';
-    }
-
-    public function start(Request $request, AuthenticationException $authException = null)
-    {
-        return $this->httpUtils->createRedirectResponse($request, $this->loginPath);
+        parent::__construct($kernel, $httpUtils, $loginPath, $useForward);
     }
 }
